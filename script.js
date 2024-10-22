a49d23e8cfd8fc9877fdd70699d08237d4487b54
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2024-10-22 23:51:28 +0700

    Updated

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
+const question = document.getElementById('question');
+const answerInput = document.getElementById('answer-input');
+const verifyBtn = document.getElementById('verify-btn');
+const result = document.getElementById('result');
+const clue = document.getElementById('clue');
+const themeBtn = document.getElementById('theme-button');
+const image = document.getElementById('image');
+const body = document.body;
+let countdownValue = 3;
 
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
-
-function calculateResult() {
-    const display = document.getElementById('display');
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
+verifyBtn.addEventListener('click', () => {
+    if (answerInput.value === '21') {
+        result.style.color = 'green';
+        const countdownInterval = setInterval(() => {
+            result.textContent = 'duh, tunggu '+ countdownValue +' detik berarti yawww!';
+            // clue.textContent = countdownValue;
+            countdownValue--;
+            if (countdownValue < 0) {
+                clearInterval(countdownInterval);
+                countdownValue = 0;
+                clue.textContent = "kocak penjumlahan aja salah? scroll ke paling bawah";
+            }
+        }, 1000);
+    } else {
+        result.textContent = 'jawaban salah kocak, coba lagi!';
+        result.style.color = 'red';
     }
+});
+
+function toggleDarkMode() {
+    body.classList.toggle('dark-mode');
+    image.classList.remove('image');
+    question.value = "Berapa umur pipi sekarang?"
 }
+
+themeBtn.addEventListener('click', toggleDarkMode);
